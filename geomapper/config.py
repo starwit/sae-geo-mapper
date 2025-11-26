4098f2414c9561125a0a006d19b164362d45da5b
flonix8 <flstanek@googlemail.com>
2025-11-26 18:13:40 +0100

    Use recently added Pydantic implementation of yaml settings source

--- geomapper/config.py
+++ geomapper/config.py
@@ -1,11 +1,14 @@
+import os
 from enum import Enum
 from typing import List, Literal, Optional
 
 from geojson_pydantic import Polygon
 from pydantic import BaseModel, Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import (BaseSettings, SettingsConfigDict,
+                               YamlConfigSettingsSource)
 from typing_extensions import Annotated
-from visionlib.pipeline.settings import LogLevel, YamlConfigSettingsSource
+from visionlib.pipeline.settings import LogLevel
+
 
 class RedisConfig(BaseModel):
     host: str = 'localhost'
@@ -57,4 +60,5 @@ class GeoMapperConfig(BaseSettings):
 
     @classmethod
     def settings_customise_sources(cls, settings_cls, init_settings, env_settings, dotenv_settings, file_secret_settings):
-        return (init_settings, env_settings, YamlConfigSettingsSource(settings_cls), file_secret_settings)
+        YAML_LOCATION = os.environ.get('SETTINGS_FILE', 'settings.yaml')
+        return (init_settings, env_settings, YamlConfigSettingsSource(settings_cls, YAML_LOCATION), file_secret_settings)
